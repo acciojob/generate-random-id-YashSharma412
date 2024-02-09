bd0a212d323bf98ca30783facf48c77ffcd04438
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2024-02-09 16:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	l = Number(l)
+
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	let arr = []
+	for (let i = 0; i < l; i++) {
+		arr.push(characters[Math.floor(Math.random()*characters.length)])
+	}
+	return arr.join("")
 }
 
 // Do not change the code below.
